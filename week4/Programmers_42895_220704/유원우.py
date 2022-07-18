73879829b3eb82323f854596aea74a7c871c6786
유원우 (Won Woo Lyu) 사원 (Data Science팀) <wwlyu@K100220421031.corp.seegene.com>
2022-07-18 20:22:48 +0900

    DP HARD

---
+++ week4/Programmers_42895_220704/유원우.py
@@ -0,0 +1,22 @@
+def solution(N, number):
+    dp = [set() for i in range(9)]
+    
+    for i in range(1, 9):
+        dp[i].add(int(str(N) * i))
+        
+        for j in range(i//2 + 1):
+            for first in dp[j]:
+                for second in dp[i-j]:
+                    dp[i].add(first + second)
+                    dp[i].add(first - second)
+                    dp[i].add(second - first)
+                    dp[i].add(first * second)
+                    if second != 0:
+                        dp[i].add(int(first/second))
+                    if first != 0:
+                        dp[i].add(int(second/first))
+
+        if number in dp[i]:
+            return i
+    
+    return -1
